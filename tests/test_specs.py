2b0cf0ebdea20b45f6f6e46f2ab8f3f7def295fa
Tobias Reiher <reiher@componolit.com>
2020-11-24 19:50:49 +0100

    Fix testing of compilability of generated code

--- tests/test_specs.py
+++ tests/test_specs.py
@@ -7,8 +7,5 @@ import pytest
 
 @pytest.mark.parametrize("spec", glob.glob("*.rflx"))
 def test_spec(spec: str, tmp_path: pathlib.Path) -> None:
-    print("start")
     subprocess.run(["rflx", "generate", "-d", tmp_path, spec], check=True)
-    print("gprbuild")
     subprocess.run(["gprbuild", "-U"], check=True, cwd=tmp_path)
-    print("done")
